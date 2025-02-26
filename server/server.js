1f291dcdafa862440166ebaae81096326902c2ed
Manmay Rawal <manmayrawal406@gmail.com>
2025-02-26 15:08:32 +0530

    crm Project

--- server/server.js
+++ server/server.js
@@ -49,6 +49,6 @@ userRouter.post("/login", userLogin);
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${PORT}`);
     dbConnect();
 });
